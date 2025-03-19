15cc7856fe47e44f3f7126d90c582d367f5615ee
Dhanush <dhanus6851@example.com>
2025-03-19 23:06:07 +0530

    setup server

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+import { useLocation } from 'react-router-dom';
 
-const chat = () => {
+let socket;
+const Chat = () => {
+  const location = useLocation(); // Use the useLocation hook
+  const [name , setName]=useState('');
+  const [room , setRoom] = useState('');
+  const ENDPOINT = 'location:5000'
+  
+  useEffect(() => {
+    if (location && location.search) {
+      const {name,room} = queryString.parse(location.search);
+      
+      socket = io(ENDPOINT);
+      setName(name);
+      setRoom(room);
+
+      console.log(socket);
+    }
+  }, [location]); // Add location as a dependency
+
   return (
     <h1>Chat</h1>
-  )
-}
+  );
+};
 
-export default chat
+export default Chat;
--- client/src/component/Join/Join.js
+++ client/src/component/Join/Join.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import { Link } from 'react-router';
+import './Join.css';
 
 const Join = () => {
+const [name , setName]=useState('');
+const [room , setRoom] = useState('');
+
+
   return (
-  <h1>Join</h1>
+ <div className="joinOuterContainer">
+  <div className="joinInnerContainer">
+    <hi className="heading">Join</hi>
+    <div><input type="text" placeholder='Enter Name' className="joinInput" onChange={(e)=>setName(e.target.value)} /></div>
+    <div><input type="text" placeholder='Enter Password' className="joinInput" onChange={(e)=>setRoom(e.target.value)} /></div>
+    <Link onClick={e=>(!name || !room)?e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+    <button className="button mt-20" type='submit'>
+     Sign in
+    </button>
+    </Link>
+  </div>
+ </div>
   )
 }
 
